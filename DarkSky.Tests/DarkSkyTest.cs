e410a2e5924ba97480afe6a4cb0d40c4645d5aa1
Armando Di Cianno <armando@dicianno.org>
2012-03-10 17:26:37 -0500

    Added support precipation call.

--- DarkSky.Tests/DarkSkyTest.cs
+++ DarkSky.Tests/DarkSkyTest.cs
@@ -1,6 +1,7 @@
 ﻿using DarkSkySharp;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace DarkSkySharp.Tests
 {
@@ -78,6 +79,24 @@ namespace DarkSkySharp.Tests
             //string expected = string.Empty; // TODO: Initialize to an appropriate value
             ForecastObject fo = darkSky.Forecast(location);
             Console.WriteLine("{0}", fo.DayPrecipitationList[0].Type);
+            //Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Precipitation
+        ///</summary>
+        [TestMethod()]
+        public void PrecipitationTest()
+        {
+            DarkSky darkSky = new DarkSky();
+            Geolocation location = new Geolocation();
+            //string expected = string.Empty; // TODO: Initialize to an appropriate value
+            List<KeyValuePair<Geolocation, int>> list = new List<KeyValuePair<Geolocation, int>>();
+            KeyValuePair<Geolocation, int> kv = new KeyValuePair<Geolocation, int>(location, 1331416523);
+            list.Add(kv);
+            PrecipitationObject po = darkSky.Precipitation(list);
+            Console.WriteLine("{0}", po.PrecipitationList[0].Type);
             //Assert.AreEqual(expected, actual);
             Assert.Inconclusive("Verify the correctness of this test method.");
         }
